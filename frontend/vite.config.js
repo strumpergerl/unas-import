f4a35a8bf4c435a2a40afd5d9c1f1734c268be8e
Strumpi <strumperger.laszlo@gmail.com>
2025-08-31 15:09:21 +0200

    fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,4 @@
-// vite.config.js
+// frontend/vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath } from 'url'
@@ -7,18 +7,19 @@ import { dirname, resolve } from 'path'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
+const isCI = process.env.CI === 'true'
+
 export default defineConfig({
   plugins: [vue()],
   server: {
     port: 5173,
     proxy: {
-      '/api': process.env.VITE_API_URL
+      '/api': process.env.VITE_API_URL || 'http://localhost:3000/api'
     }
   },
   base: './',
   build: {
-    // a build kimenete a backend public mappájába kerül
-    outDir: resolve(__dirname, '../backend/public'),
+    outDir: isCI ? 'dist' : resolve(__dirname, '../backend/public'),
     emptyOutDir: true
   }
 })

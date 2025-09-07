0bf56a13d106b3a87c85cad0c24e45b43a801dcc
Strumpi <strumperger.laszlo@gmail.com>
2025-09-07 20:43:52 +0200

    vercel fixes

--- api/[[...all]].js
+++ api/[[...all]].js
@@ -1,7 +1,18 @@
 // api/[[...all]].js
 console.log('[[...all]] handler loaded');
+const express = require('express');
+const apiRouter = require('../backend/src/api');
 
-// Minimal Vercel function for debug
-module.exports = (req, res) => {
-  res.json({ ok: true, path: req.url, method: req.method });
-};
+const app = express();
+
+// (Opcionális) /api prefix levágása, ha így érkezik a req.url
+app.use((req, _res, next) => {
+  if (req.url.startsWith('/api/')) req.url = req.url.slice(4);
+  next();
+});
+
+// Mountold az Express routert ( /config, /rates, /logs, stb. )
+app.use(apiRouter);
+
+// Vercel Node handler: (req, res) -> app(req, res)
+module.exports = (req, res) => app(req, res);
